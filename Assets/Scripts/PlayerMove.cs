120f08f38c2756ff484db1f5430f76e424119610
Yvan Roux <yvis99@gmail.com>
2024-03-03 16:50:28 +0100

    jump

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,10 +6,19 @@ public class PlayerMove : MonoBehaviour
 {
     public float speed = 10;
     public float rotationSpeed = 100;
+    public float jumpForce = 10;
     
     private float _horizontalInput;
     private float _verticalInput;
     private float _horizontalMouseInput;
+    
+    private Rigidbody _rigidBody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
     
     // Update is called once per frame
     void Update()
@@ -22,5 +31,10 @@ public class PlayerMove : MonoBehaviour
         
         transform.Translate(speed * Time.deltaTime * direction);
         transform.Rotate(Vector3.up, _horizontalMouseInput * rotationSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
