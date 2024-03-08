cdbc9f30f8817d824c0c5f8661ad708158b83fab
Yvan Roux <yvis99@gmail.com>
2024-03-08 22:38:06 +0100

    super collectibles spawn and disapear auto

--- Assets/Scripts/CollectibleManagerScript.cs
+++ Assets/Scripts/CollectibleManagerScript.cs
@@ -7,9 +7,13 @@ using Random = UnityEngine.Random;
 public class CollectibleManagerScript : MonoBehaviour
 {
     public List<Transform> collectibleSpawns;
-    public GameObject collectiblePrefab;
+    public GameObject classicCollectiblePrefab;
+    public GameObject superCollectiblePrefab;
+    public float superCollectibleSecondsBeforeNextSpawn = 8;
+    public float superCollectibleSecondsBeforeDisappear = 5;
     
     private GameControllerScript _gameController;
+    private GameObject _currentSuperCollectible;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +22,15 @@ public class CollectibleManagerScript : MonoBehaviour
             throw new MissingFieldException("collectibleSpawns cannot be empty.");
         }
         _gameController = GameObject.FindWithTag("GameController").GetComponent<GameControllerScript>();
-        SpawnCollectible();
+        SpawnCollectible(classicCollectiblePrefab);
+        StartCoroutine(SpawnSuperCollectible());
     }
 
     public void CollectibleWasPickedUp(GameObject collectible)
     {
         _gameController.PlayerPickedUpCollectibe();
         Destroy(collectible);
-        SpawnCollectible();
+        SpawnCollectible(classicCollectiblePrefab);
     }
     
     public void SuperCollectibleWasPickedUp(GameObject collectible)
@@ -34,12 +39,24 @@ public class CollectibleManagerScript : MonoBehaviour
         Destroy(collectible);
     }
     
-    private void SpawnCollectible()
+    private GameObject SpawnCollectible(GameObject prefabToSpawn)
     {
         int spawnIndex = Random.Range(0, collectibleSpawns.Count);
-        GameObject collectible = Instantiate(collectiblePrefab, collectibleSpawns[spawnIndex].position, Quaternion.identity);
-        var collectibleScript = collectible.GetComponent<CollectibleSrcipt>();
-        collectibleScript.SetCollectibleManager(this);
+        GameObject collectible = Instantiate(prefabToSpawn, collectibleSpawns[spawnIndex].position, Quaternion.identity);
+        collectible.GetComponent<CollectibleSrcipt>().SetCollectibleManager(this);
+        return collectible;
+    }
+    
+    //coroutine to spawn super collectible every 10 seconds
+    private IEnumerator SpawnSuperCollectible()
+    {
+        while (true)
+        {
+            Destroy(_currentSuperCollectible);
+            yield return new WaitForSeconds(superCollectibleSecondsBeforeNextSpawn);
+            _currentSuperCollectible = SpawnCollectible(superCollectiblePrefab);
+            yield return new WaitForSeconds(superCollectibleSecondsBeforeDisappear);
+        }
     }
     
     
