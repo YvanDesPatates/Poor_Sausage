1d292207325c665ce0278ced63929ebc60c562db
Yvan Roux <yvis99@gmail.com>
2024-03-09 18:10:40 +0100

    adding lights changes before bomb is throw

--- Assets/Scripts/BombObstacleScript.cs
+++ Assets/Scripts/BombObstacleScript.cs
@@ -1,23 +1,39 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BombObstacleScript : MonoBehaviour
 {
-    public GameObject bomb;
+    public Rigidbody bombRb;
+    public Light spotLight;
     public float secondsBeforeBombFalls = 3;
+    public float minimalSpotAngle = 10;
 
-    private Rigidbody _bombRb;
         
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(BeforeThrowBomb());
         StartCoroutine(ThrowBomb());
-        _bombRb = bomb.GetComponent<Rigidbody>();
     }
 
+    private IEnumerator BeforeThrowBomb()
+    {
+        float stepsDuration = secondsBeforeBombFalls / 3;
+        float actualSpotAngle = spotLight.spotAngle;
+        float spotAngleReduction = (actualSpotAngle - minimalSpotAngle) / 3;
+        for (int i = 0; i < 3; i++)
+        {
+            yield return new WaitForSeconds(stepsDuration);
+            actualSpotAngle -= spotAngleReduction;
+            spotLight.spotAngle = actualSpotAngle;
+        }
+        spotLight.color = Color.red;
+    }
+    
     private IEnumerator ThrowBomb()
     {
         yield return new WaitForSeconds(secondsBeforeBombFalls);
-        _bombRb.isKinematic = false;
+        bombRb.isKinematic = false;
     }
 }
