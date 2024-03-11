ff914abe06c5c84d1ea29048b02cb0bd01f09e6b
Yvan Roux <yvis99@gmail.com>
2024-03-11 14:19:29 +0100

    trapSpawner

--- Assets/Scripts/traps/TrapManagercript.cs
+++ Assets/Scripts/traps/TrapManagercript.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class TrapManagercript : MonoBehaviour
 {
+    public List<GameObject> trapPrefabs;
+    public float spawnRateInS = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnTrap(trapPrefabs[0]);
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private IEnumerator SpawnTraps()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRateInS);
+            int index = Random.Range(0, trapPrefabs.Count);
+            SpawnTrap(trapPrefabs[index]);
+        }
+    }
+    
+    private void SpawnTrap(GameObject trapPrefab)
+    {
+        Instantiate(trapPrefab);
     }
 }
