bd1f923c88eab3bbf6204f9f2f00941aeabf841c
Yvan Roux <yvis99@gmail.com>
2024-03-16 19:03:46 +0100

    laser deactivation and reactivation

--- Assets/Scripts/traps/LaserTrapScript.cs
+++ Assets/Scripts/traps/LaserTrapScript.cs
@@ -1,10 +1,18 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 public class LaserTrapScript : MonoBehaviour
 {
+    public List<GameObject> deadlyLasers;
+    public List<GameObject> preventionLasers;
+    public float pauseBetweenLasersInS = 2f;
+    public float laserDurationInS = 5f;
+
     private static List<Vector3> _initialPlacements;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,22 +26,45 @@ public class LaserTrapScript : MonoBehaviour
 
             if (_initialPlacements.Count == 0)
             {
-                throw new  UnityException("laser trap need at least one spawn point! (with tag Laser Spawns)");
+                throw new UnityException("laser trap need at least one spawn point! (with tag Laser Spawns)");
             }
         }
 
         InitialRandomPlacement();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        StartCoroutine(LaserRoutine());
     }
 
     private void InitialRandomPlacement()
     {
         int index = Random.Range(0, _initialPlacements.Count);
         transform.SetPositionAndRotation(_initialPlacements[index], transform.rotation);
+    }
+
+    private IEnumerator LaserRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(laserDurationInS);
+            deadlyLasers.ForEach(laser => laser.SetActive(false));
+
+            //prevention phase while lasers are not deadly
+            if (pauseBetweenLasersInS >= 3)
+            {
+                yield return new WaitForSeconds(pauseBetweenLasersInS - 3);
+                for (int i = 0; i < 3; i++)
+                {
+                    preventionLasers.ForEach(laser => laser.SetActive(true));
+                    yield return new WaitForSeconds(0.5f);
+                    preventionLasers.ForEach(laser => laser.SetActive(false));
+                    yield return new WaitForSeconds(0.33f);
+                }
+            }
+            else
+            {
+                yield return new WaitForSeconds(pauseBetweenLasersInS);
+            }
+
+            deadlyLasers.ForEach(laser => laser.SetActive(true));
+        }
     }
 }
