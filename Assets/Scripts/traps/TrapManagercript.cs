477ccfa1b26d01048ae7928385c85e9ba166fef2
Yvan Roux <yvis99@gmail.com>
2024-03-25 10:23:33 +0100

    subscribe concerned traps

--- Assets/Scripts/traps/TrapManagercript.cs
+++ Assets/Scripts/traps/TrapManagercript.cs
@@ -2,29 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrapManagercript : MonoBehaviour
+public class TrapManagercript : MonoBehaviour, IGameOverObserver
 {
     public List<GameObject> trapPrefabs;
     public float spawnRateInS = 5f;
+    
+    private GameControllerScript _gameController;
+    private bool _doSpawnTraps = true;
     // Start is called before the first frame update
     void Start()
     {
+        _gameController = GameObject.FindWithTag("GameController").GetComponent<GameControllerScript>();
+        _gameController.SubscribeToGameOverNotification(this);
         SpawnTrap(trapPrefabs[0]);
         StartCoroutine(SpawnTraps());
     }
     
     private IEnumerator SpawnTraps()
     {
-        while (true)
+        while (_doSpawnTraps)
         {
             yield return new WaitForSeconds(spawnRateInS);
             int index = Random.Range(0, trapPrefabs.Count);
-            SpawnTrap(trapPrefabs[index]);
+            if (_doSpawnTraps)
+            {
+                SpawnTrap(trapPrefabs[index]);
+            }
         }
     }
     
     private void SpawnTrap(GameObject trapPrefab)
     {
-        Instantiate(trapPrefab);
+        var trap = Instantiate(trapPrefab);
+        var observer = trap.GetComponent<IGameOverObserver>();
+        if (observer != null)
+        {
+            _gameController.SubscribeToGameOverNotification(observer);
+        }
+    }
+
+    public void GameOverNotification()
+    {
+        _doSpawnTraps = false;
     }
 }
